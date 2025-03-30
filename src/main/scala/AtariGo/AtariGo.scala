c1badbe3bd5ab82c80d5efbcf00ae3185d9b70b1
D-Figueira <diogoaf.2004@gmail.com>
2025-03-30 18:54:09 +0100

    initial commit

--- src/main/scala/AtariGo/AtariGo.scala
+++ src/main/scala/AtariGo/AtariGo.scala
@@ -6,11 +6,18 @@ import AtariGoUtils.{showPrompt, getUserInput}
 type Board = List[List[Stone]]
 type Coord2D = (Int, Int)
 
-case class AtariGo(board: Board) //implementar os métodos para adicionar/meter peças + obter as coordenadas vazias
+case class AtariGo(board: Board) { //implementar os métodos para adicionar/meter peças + obter as coordenadas vazias
+  def emptyPlaces: List[Coord2D] = AtariGo.getListOfEmptyPlaces(board)
+}
 
 object AtariGo extends App {
-  //Game(createBoard(9))
-  println(getListOfEmptyPlaces(createBoard(9)))
+  val board = AtariGo.createBoard(9)
+  val game = AtariGo(board)
+  val emptyPlaces = game.emptyPlaces
+  println(board)
+  println(emptyPlaces)
+  println(validCoord((2,2),board))
+
 
   def createRow(size: Int): List[Stone] = {
     if (size == 0) Nil
@@ -22,7 +29,7 @@ object AtariGo extends App {
     else createRow(9) :: createBoard(size - 1)
   }
 
-  def getListOfEmptyPlaces(lista: List[List[Stone]]): List[Coord2D] = {
+  def getListOfEmptyPlaces(lista: Board): List[Coord2D] = {
     def aux_row(rows: List[List[Stone]], rowIndex: Int, acc: List[Coord2D]): List[Coord2D] = rows match {
       case Nil => acc
       case head :: tail =>
@@ -38,6 +45,12 @@ object AtariGo extends App {
         aux_columns(tail, rowIdx, colIdx + 1, acc)
     }
     aux_row(lista, 0, Nil)
+  }
+
+  def validCoord(coord:Coord2D,board: Board):Boolean = {
+   val empty_coords = getListOfEmptyPlaces(board)
+    if (empty_coords.contains(coord)) true
+    else false
   }
 
 //  private def putPiece(board: Board, coord: Coord2D, stone: Stone): Board = {
