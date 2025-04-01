17afbee10a2ccf47e21da15f2cc4dc6888553b16
D-Figueira <diogoaf.2004@gmail.com>
2025-04-01 09:10:13 +0100

    commit

--- src/main/scala/AtariGo/AtariGo.scala
+++ src/main/scala/AtariGo/AtariGo.scala
@@ -10,31 +10,16 @@ case class AtariGo(board: Board) { //implementar os métodos para adicionar/mete
   def emptyPlaces: List[Coord2D] = AtariGo.getListOfEmptyPlaces(board)
 }
 
-object AtariGo extends App {
-  val board = AtariGo.createBoard(9)
-  val game = AtariGo(board)
-  val emptyPlaces = game.emptyPlaces
-  println(board)
-  println(emptyPlaces)
-  println(validCoord((2,2),board))
-
-
-  def createRow(size: Int): List[Stone] = {
-    if (size == 0) Nil
-    else Stone.Empty :: createRow(size - 1)
-  }
-
-  def createBoard(size: Int): Board = {
-    if (size == 0) Nil
-    else createRow(9) :: createBoard(size - 1)
-  }
+object AtariGo{
+  def createBoard(size: Int): Board =
+    List.fill(size)(List.fill(size)(Stone.Empty))
 
   def getListOfEmptyPlaces(lista: Board): List[Coord2D] = {
     def aux_row(rows: List[List[Stone]], rowIndex: Int, acc: List[Coord2D]): List[Coord2D] = rows match {
       case Nil => acc
       case head :: tail =>
         val newCoords = aux_columns(head, rowIndex, 0, Nil)
-        aux_row(tail, rowIndex + 1, acc ++ newCoords)
+        aux_row(tail, rowIndex + 1, acc ++ newCoords) // ++ para concatenação das listas
     }
 
     def aux_columns(columns: List[Stone], rowIdx: Int, colIdx: Int, acc: List[Coord2D]): List[Coord2D] = columns match {
@@ -48,14 +33,42 @@ object AtariGo extends App {
   }
 
   def validCoord(coord:Coord2D,board: Board):Boolean = {
+    // adicionar a verificação relativamente aos graus de liberdade
    val empty_coords = getListOfEmptyPlaces(board)
     if (empty_coords.contains(coord)) true
     else false
   }
 
-//  private def putPiece(board: Board, coord: Coord2D, stone: Stone): Board = {
+  def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) = lstOpenCoords match {
+    case Nil =>
+      throw new IllegalArgumentException("Não há jogadas possíveis.")
+
+    case _ =>
+      val (randomIndex, nextRand) = rand.nextInt(lstOpenCoords.length)
+      val selectedCoord = lstOpenCoords(randomIndex)
+      (selectedCoord, nextRand)
+  }
+
+  //  private def putPiece(board: Board, coord: Coord2D, stone: Stone): Board = {
 //    board.updated(index, alteração a fazer no tabuleiro)
 //  }
+}
 
+object AtariGoApp extends App {
+  val board = AtariGo.createBoard(9)
+  val game = AtariGo(board)
+
+  println("Tabuleiro inicial:")
+  println(board)
+
+  println("\nPosições vazias:")
+  println(game.emptyPlaces)
+
+  val r = MyRandom(System.currentTimeMillis())
+  println("\nPosição aleatória + random seed:")
+  println(AtariGo.randomMove(game.emptyPlaces,r))
+
+  val coord = (2, 2)
+  println(s"\nCoordenada $coord é válida? ${AtariGo.validCoord(coord, board)}")
 }
 
--- src/main/scala/AtariGo/MyRandom.scala
+++ src/main/scala/AtariGo/MyRandom.scala
@@ -1,18 +1,19 @@
 package AtariGo
 
-case class MyRandom(seed: Long) extends RandomWithState {
-  def nextInt: (Int, RandomWithState) = {
+case class MyRandom(seed: Long) {
+  def randomInt: (Int, MyRandom) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL //origina uma nova semente
     val nextRandom = MyRandom(newSeed) //cria uma instância MyRandom com a semente previamente criada
     val n = (newSeed >>> 16).toInt //extrai os 32 bits superiores de newSeed
     (n, nextRandom) //retorna uma tupla com o número gerado e a nova semente.
   }
-  def nextInt(n: Int): (Int, RandomWithState) = { //calcula um número aleatório de [0-n]
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL  //origina uma nova semente
-    val nextRandom = MyRandom(newSeed)  //cria uma instância MyRandom com a semente previamente criada
+
+  def nextInt(n: Int): (Int, MyRandom) = { //calcula um número aleatório de [0-n]
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL //origina uma nova semente
+    val nextRandom = MyRandom(newSeed) //cria uma instância MyRandom com a semente previamente criada
     val nn = ((newSeed >>> 16).toInt) % n
-    (if(nn<0) -nn else nn, nextRandom) //nn for negativo, converte para positivo com -nn
+    (if (nn < 0) -nn else nn, nextRandom) //nn for negativo, converte para positivo com -nn
+  }
   }
   //Cada nextRandom carrega a seed atualizada para a próxima operação, garantindo que a sequência seja sempre reproduzível!
   //Deste modo a sequência não será sempre a mesma
-}
